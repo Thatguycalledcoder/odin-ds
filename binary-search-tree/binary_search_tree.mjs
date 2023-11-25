f4028ab18ffcc9659fc1d4695395085f79b2e81f
Thatguycalledcoder <quarshiedavid62@gmail.com>
2023-11-24 19:27:00 -0600

    Fixed buildTree function. Added test file.

--- binary-search-tree/binary_search_tree.mjs
+++ binary-search-tree/binary_search_tree.mjs
@@ -1,20 +1,22 @@
 import Node from "./node.mjs";
 
-class BinarySearchTree {
+export default class BinarySearchTree {
     constructor() {
         this.root = new Node();
     }
 
     buildTree(data) {
         data = this.data_preprocess(data);
-        const mid = data.length / 2;
-        this.root.value = mid;
+        console.log(data);
+        const mid = Math.floor(data.length / 2);
+        const length = data.length
+        this.root.value = data[mid];
 
         // build left subtree
-        this.root.leftNode = this.buildSubtree(data.slice(0, mid), 0, mid-1);
+        this.root.leftNode = this.buildSubtree(data, 0, mid-1);
 
         // build right subtree
-        this.root.rightNode = this.buildSubtree(data.slice(mid + 1, data.length), mid + 1, data.length - 1);
+        this.root.rightNode = this.buildSubtree(data, mid + 1, length - 1);
 
         return this.root;
     }
@@ -35,10 +37,10 @@ class BinarySearchTree {
     buildSubtree(data, start, end) {
         if (start > end) return null;
 
-        const mid = (start + end) / 2;
+        const mid = Math.floor((start + end) / 2);
         let node = new Node(data[mid]);
-        node.leftNode = this.buildSubtree(data.slice(start, mid), start, mid - 1);
-        node.rightNode = this.buildSubtree(data.slice(mid+1, end+1), mid+1, end);
+        node.leftNode = this.buildSubtree(data, start, mid-1);
+        node.rightNode = this.buildSubtree(data, mid+1, end);
 
         return node;
     }
---
+++ binary-search-tree/test.mjs
@@ -0,0 +1,22 @@
+import './binary_search_tree.mjs';
+import BinarySearchTree from './binary_search_tree.mjs';
+
+const data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const tree = new BinarySearchTree();
+let node = tree.buildTree(data);
+
+function prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.rightNode !== null) {
+      prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.leftNode !== null) {
+      prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+prettyPrint(node);
